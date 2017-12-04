5a7bc9c8249331586fe11fabeebff3e40c75630e
mplanas <mplanas@visionbanco.com>
2017-12-04 16:58:09 -0300

    tutorial de Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Ironman' },
+    { id: 12, name: 'Blackwidow' },
+    { id: 13, name: 'Hulk' },
+    { id: 14, name: 'Thor' },
+    { id: 15, name: 'Haweye' },
+    { id: 16, name: 'Captian America' },
+    { id: 17, name: 'Scarlet Witch' },
+    { id: 18, name: 'Vision' },
+    { id: 19, name: 'Black Panther' },
+    { id: 20, name: 'Spiderman' }
+];
