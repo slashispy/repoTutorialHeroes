6fa25c749c9980358812a87dccc8941d526fe0be
mplanas <mplanas@visionbanco.com>
2017-12-19 16:52:55 -0300

    tutorial finalizado hasta el cap 7

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,11 +12,11 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
+  // selectedHero: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  // onSelect(hero: Hero): void {
+   // this.selectedHero = hero;
+  // }
 
   constructor(private heroService: HeroService) { }
 
